5fe7c8b83faac276870ff9374c17d91290f5f743
Olatoye Daramola <daramolatoye@gmail.com>
2021-09-11 19:44:53 +0100

    ...

---
+++ Creating_Tables/template.sql
@@ -0,0 +1,19 @@
+-- Creating tables --
+
+/**
+create table table_name(
+
+	_DOMAIN INTEGRITY CONSTRAINT:_
+    
+	column_name datatype optional constraint,
+    column_name datatype optional constraint,
+    column_name datatype optional constraint,
+    .......
+    constraint constraint_name constraint_type(column_name)
+    
+    _constraint_name can't be repeated in any two columns_
+);
+**/
+
+
+create database demo;
